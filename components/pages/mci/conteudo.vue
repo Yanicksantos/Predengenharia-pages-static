de965b2b7a3fe84ca3cbd5b5ed5dfb852bb4e14c
PRED <yanickdev@outlook.com>
2023-11-06 09:33:56 -0300

    ajuste serie mci youtube

---
+++ components/pages/mci/conteudo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="px-4 py-6">
+            <p class="text-caption text-center font-weight-black">Certifique-se que a RBM® (Rice &Beans Maintenance), está, de fato, implementada antes de investir tempo e energia na RCM® (Reliability Centered maintenance). <br>A gente te explica como:</p>
+            
+            <v-img src="/img/mci/3.png" class="rounded my-4"></v-img>
+            <p class="text-body-2">Desenvolvido pela Pred Engenharia, o método MCI, trouxe uma perspectiva inovadora que, diferente de outros métodos existentes no mercado, promove a integração da cadeia de valores da Manutenção através de atribuições e entregáveis claramente definidos e auditáveis.</p>
+            <v-img src="/img/mci/4.jpeg" class="rounded my-4"></v-img>
+
+            <p class="text-body-2">No modelo MCI as manutenções preventiva sob condição e corretiva planejada são integradas de forma harmoniosa, em um ciclo fechado, e sustentada por uma ferramenta inteligente denominada PERIGÔMETRO ®. Essa ferramenta, desenvolvida pela Pred, faz a avaliação do que realmente deve entrar nas programações de manutenção, dando equilíbrio ao recurso.</p>
+            <p class="mt-4 text-body-2"> É nesse ciclo fechado que conseguimos garantir a disponibilidade e previsibilidade com o menor custo possível, pois se trabalha com a visão mínima de 4 semanas à frente (S+4).</p>
+            <v-img src="/img/mci/5.jpeg" class="rounded my-4"></v-img>
+            <p class="mt-4 text-body-2"> A frase que rege a metodologia MCI é “o simples é o máximo da sofisticação”. Isso porque todas as ações que sustentam o método são simples. No entanto, devido a sua doutrina estruturada e organizada, promove solidez e previsibilidade.</p>
+        </div>
+    </div>
+</template>
